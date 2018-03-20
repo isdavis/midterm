5182d6001ffecc21603419028acfda329656145a
isdavis <isdavis@wustl.edu>
2018-03-20 09:12:07 -0500

    EAP function

--- RaschDevFile.R
+++ RaschDevFile.R
@@ -49,3 +49,21 @@ rasch.prior<-function(theta) {
 #Small tests: 
 rasch.like(i, 2)
 rasch.prior(2)
+
+#EAP function. This defaults to an integral from -6 to 6. 
+rasch.EAP<-function(stud, lower=-6, upper=6) {
+#Denominator function: L(theta|y)*pi(theta)
+  denominator<-function(stud, theta) {
+    rasch.like(stud, theta)*rasch.prior(theta)
+  }
+#numerator function: L(theta|y)*pi(theta)*theta
+  numerator<-function(stud, theta) {
+    rasch.like(stud, theta)*rasch.prior(theta)*theta
+  }
+  estimateN<-integrate(numerator, stud=stud, lower, upper)
+  print(estimateN)
+  estimateD<-integrate(denominator, stud=stud, lower, upper)
+  print(estimateD)
+  return(estimateN/estimateD)
+}
+
